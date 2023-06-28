41e998485adc4551a8f29317a9ed1f1cd38f68fa
Ankit <uankitu@gmail.com>
2023-06-28 12:41:44 +0530

    Coded Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static String getHelloWorld() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args){
+         String helloWorld = getHelloWorld();
+         System.out.println(helloWorld);
+    }
+}
