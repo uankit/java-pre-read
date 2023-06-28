1517d4611f8a72d38e1538be74735e56b6e639da
Ankit <uankitu@gmail.com>
2023-06-28 13:05:49 +0530

    Coded Twofer

---
+++ Twofer.java
@@ -0,0 +1,7 @@
+public class Twofer {
+    public String twofer(String name) {
+        if(name == null || name.equals(""))
+            return "One for you, one for me.";
+        return "One for " + name + ", one for me.";
+    }
+}
