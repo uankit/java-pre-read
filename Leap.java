eb55d5004920cdd394e091b58a7cb8b4bd581572
Ankit <uankitu@gmail.com>
2023-06-28 13:23:12 +0530

    Coded Leap

---
+++ Leap.java
@@ -0,0 +1,13 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if(year%4 == 0)
+            if(year%100 == 0){
+                return year % 400 == 0;
+            }else{
+                return true;
+            }
+        return false;
+    }
+
+}
